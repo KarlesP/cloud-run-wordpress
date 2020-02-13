703d06dcd096ecb84a954df2c7527f09a103bcb8
Mr Koios <george.nostradamos@gmail.com>
2020-02-13 09:40:06 +0200

    Update build-deploy.sh

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,6 +1,6 @@
-gcloud builds submit --tag gcr.io/cr-test-wordpress/wp:v1
-gcloud beta run deploy --image gcr.io/cr-test-wordpress/wp:v1 --add-cloudsql-instances mysql
+gcloud builds submit --tag gcr.io/WrdPrs/wp:v1
+gcloud beta run deploy --image gcr.io/WrdPrs/wp:v1 --add-cloudsql-instances storage1
 gcloud beta run services delete wp
-gcloud beta run deploy wp --image gcr.io/cr-test-wordpress/wp:v1 \
-  --add-cloudsql-instances mysql \
-  --update-env-vars DB_HOST='127.0.0.1',DB_NAME=wordpress,DB_USER=wordpress,DB_PASSWORD=wordpress,CLOUDSQL_INSTANCE='cr-test-wordpress:us-central1:mysql'
+gcloud beta run deploy wp --image gcr.io/WrdPrs/wp:v1 \
+  --add-cloudsql-instances storage1 \
+  --update-env-vars DB_HOST='127.0.0.1',DB_NAME=wordpress,DB_USER=wordpress,DB_PASSWORD=toor,CLOUDSQL_INSTANCE='WrdPrs:us-central1:storage1'
